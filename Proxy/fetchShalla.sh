41d7f1756751a591b568f885f2052bfe6a7e3ae1
Unknown <kukshaus@dial.de>
2016-11-11 21:21:50 +0100

    Shell Branding :-)

--- Proxy/fetchShalla.sh
+++ Proxy/fetchShalla.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 ### BEGIN INIT INFO
-# Provides:          fetchShalla
+# Provides:          
 # Required-Start:    
 # Required-Stop:     
 # Default-Start:     2 3 4 5
@@ -9,7 +9,18 @@
 # Description:       Holt sich die aktuelle Shallalist Blacklist
 ### END INIT INFO
 
+echo "+-------------------------------------------------------------------+"
+echo "|                  ______      _                                    |"
+echo "|                 (_____ \    (_)                                   |"
+echo "|                  _____) )___ _ _   _ _   _ _   _                  |"
+echo "|                 |  ____/ ___) | | | | | | ( \ / )                 |"
+echo "|                 | |   | |   | |\ V /| |_| |) X (                  |"
+echo "|                 |_|   |_|   |_| \_/  \____(_/ \_)                 |"
+echo "|                                                                   |"
+echo "+-------------------------------------------------------------------+"
+
 log() {
+	echo "$(date '+%b %d %H:%M:%S') $(hostname) shalla: $1"
 	echo "$(date '+%b %d %H:%M:%S') $(hostname) shalla: $1" >> /etc/squidguard/shalla.log
 }
 
